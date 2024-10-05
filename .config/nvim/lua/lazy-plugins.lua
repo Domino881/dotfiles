c9fcbd471daf8722c21fd2dd8f2cb0d86300b3e5
Domino881 <domino881@gmail.com>
2024-10-05 19:25:50 +0100

    plugin list cleanup

--- .config/nvim/lua/lazy-plugins.lua
+++ .config/nvim/lua/lazy-plugins.lua
@@ -6,33 +6,47 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Setup lazy.nvim
-require("lazy").setup({
+-- Setup the plugin manager
+require("lazy").setup{
+
     'nvim-lua/plenary.nvim',               -- utilities used by other plugins
-    require 'plugins.gruvbox',             -- colorscheme
-    require 'plugins.lualine',             -- bottom status line
-    require 'plugins.treesitter',          -- for better syntax highlighting etc.
-    require 'plugins.telescope',           -- search utility
-    require 'plugins.which-key',           -- shows hints for keymaps
-    require 'plugins.lsp',                 -- errors / linting of code
-    require 'plugins.cmp',                 -- autocompletion
-    require 'plugins.conform',             -- auto formatting of code
-    require 'plugins.alpha',               -- home screen
-    require 'plugins.gitsigns',            -- git diff lines in signcolumn
-    require 'plugins.harpoon',             -- jumping between project files
-    require 'plugins.ufo-fold',            -- nicer looking folds
-    require 'plugins.treesitter-context',  -- context line for long functions
-    require 'plugins.ult-autopair',        -- automatic brackets
-    require 'plugins.undotree',            -- for viewing undo history
-    require 'plugins.luasnip',             -- snippets
-    require 'plugins.molten-ipython',      -- jupyter-like python execution
-    require 'plugins.image',               -- drawing images inside the terminal
-    'lervag/vimtex',                       -- LaTeX integration
-    { 'numToStr/Comment.nvim',                       config = true }, -- commenting of lines
-    { "nvim-treesitter/nvim-treesitter-textobjects", event = "VeryLazy" },
-    { 'lukas-reineke/indent-blankline.nvim',         main = 'ibl',    config = true }, -- scope indicators
-    'tpope/vim-surround',                  -- bracket / quote vim motions
-    'romainl/vim-cool',                    -- auto :nohl
-    'farmergreg/vim-lastplace',            -- opens files at the last place
-    'tpope/vim-fugitive',                  -- git utilities
-})
+
+    -- Options for these plugins are located in .config/nvim/lua/plugins/
+    require 'plugins.gruvbox',            -- colorscheme
+    require 'plugins.lualine',            -- bottom status line
+    require 'plugins.treesitter',         -- for better syntax highlighting etc.
+    require 'plugins.telescope',          -- search utility
+    require 'plugins.which-key',          -- shows hints for keymaps
+    require 'plugins.lsp',                -- errors / linting of code
+    require 'plugins.cmp',                -- autocompletion
+    require 'plugins.conform',            -- auto formatting of code
+    require 'plugins.alpha',              -- home screen
+    require 'plugins.gitsigns',           -- git diff lines in signcolumn
+    require 'plugins.harpoon',            -- jumping between project files
+    require 'plugins.ufo-fold',           -- nicer looking folds
+    require 'plugins.treesitter-context', -- context line for long functions
+    require 'plugins.ult-autopair',       -- automatic brackets
+    require 'plugins.undotree',           -- for viewing undo history
+    require 'plugins.luasnip',            -- snippets
+    require 'plugins.molten-ipython',     -- jupyter-like python execution
+    require 'plugins.image',              -- drawing images inside the terminal
+
+    -- Plugins below are imported with little/no extra options
+    'lervag/vimtex',                      -- LaTeX integration
+    {
+        'numToStr/Comment.nvim',          -- commenting of lines
+        config = true
+    },
+    {
+        "nvim-treesitter/nvim-treesitter-textobjects", -- vim motions for functions/classes
+        event = "VeryLazy"
+    },
+    {
+        'lukas-reineke/indent-blankline.nvim', -- scope indicators
+        main = 'ibl', config = true
+    },
+    'tpope/vim-surround',                 -- bracket / quote vim motions
+    'romainl/vim-cool',                   -- auto :nohl
+    'farmergreg/vim-lastplace',           -- opens files at the last place
+    'tpope/vim-fugitive',                 -- git utilities
+}
